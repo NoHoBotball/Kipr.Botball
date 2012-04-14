ea7fd94b26a47f1e7b8cac5bcdb6c3de51980d9c
Jake King <imjake9@gmail.com>
2012-04-13 17:44:41 -0700

    Make block input customizable.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 import java.util.List;
 
+import cbccore.sensors.buttons.AButton;
+import cbccore.sensors.buttons.BButton;
+import cbccore.sensors.buttons.BlackButton;
+
 import regionals2012.TaskRunner;
 
 import utils.Constants.Block;
@@ -12,8 +16,23 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		
-		Block[] blocks = new Block[] {Block.BLUE, Block.RED, Block.YELLOW};
-		List<Task> blockChain = Task.getTaskChain(blocks, false);
+		Block[] blocks = new Block[3];
+		
+		for (int i = 0; i < 3; i++) {
+			
+			System.out.println("Input block " + i + " (A = RED, B = YELLOW, BLACK = BLUE)");
+			boolean a = false, b = false;
+			while ((a = new AButton().isNotPushed()) && (b = new BButton().isNotPushed()) && (new BlackButton().isNotPushed()));
+			if (a)
+				blocks[i] = Block.RED;
+			else if (b)
+				blocks[i] = Block.YELLOW;
+			else
+				blocks[i] = Block.BLUE;
+			
+		}
+		
+		List<Task> blockChain = Task.getTaskChain(blocks, blocks[0] == Block.RED);
 		
 		TaskRunner blockTask = new TaskRunner(blockChain);
 		blockTask.run();
