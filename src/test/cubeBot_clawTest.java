98639ab698c82b0a39b625e200d4e12915db9c3d
Jae Kim <jaekim708@gmail.com>
2012-04-13 17:09:54 -0700

    Does this work now?
    It's a test program to find the right servo programs.

---
+++ src/test/cubeBot_clawTest.java
@@ -0,0 +1,55 @@
+import cbccore.motors.Servo;
+import cbccore.sensors.buttons.*;
+import cbccore.create.Create;
+import cbccore.create.CreateConnectException;
+import cbccore.movement.plugins.MovementPlugin;
+import cbccore.movement.plugins.create.CreateMovementPlugin;
+import cbccore.movement.DriveTrain;
+
+class Main {
+	public static void main(String[] args) {
+		MovementPlugin plugin = null;
+		try {
+			plugin = new CreateMovementPlugin(new Create());
+		} catch(CreateConnectException e) {
+			System.out.println("Create failed to connect.");
+			System.exit(1);
+		}
+		DriveTrain dt= new DriveTrain(plugin);
+		Servo servoL = new Servo(0);
+		Servo servoR = new Servo(3);
+		Servo servoClaw = new Servo(1);
+		UpButton upButton = new UpButton();
+		DownButton downButton = new DownButton(); 
+		LeftButton leftButton = new LeftButton();
+		RightButton rightButton = new RightButton();
+		BlackButton blackButton = new BlackButton();
+		servoL.enable();
+		servoR.enable();
+		servoClaw.enable();
+		while(blackButton.getValue() == true){
+			while(upButton.getValue() == true){
+				System.out.println(servoL.getPosition() + " " + servoR.getPosition());
+				servoL.setPosition(servoL.getPosition() + 30);
+				servoR.setPosition(servoR.getPosition() - 30);
+				try { Thread.sleep(100); } catch (Exception e) {}
+			}
+			while(downButton.getValue() == true){
+				System.out.println(servoL.getPosition() + " " + servoR.getPosition());
+				servoL.setPosition(servoL.getPosition() - 30);
+				servoR.setPosition(servoR.getPosition() + 30);
+				try { Thread.sleep(100); } catch (Exception e) {}
+			}
+			while(leftButton.getValue() == true){
+				System.out.println(servoClaw.getPosition());
+				servoClaw.setPosition(servoClaw.getPosition() + 30);
+				try { Thread.sleep(100); } catch (Exception e) {}
+			}
+			while(rightButton.getValue() == true){
+				System.out.println(servoClaw.getPosition());
+				servoClaw.setPosition(servoClaw.getPosition() - 30);
+				try { Thread.sleep(100); } catch (Exception e) {}
+			}
+		}
+	}
+}
