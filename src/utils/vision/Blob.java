81c21c6465469f2f8a6f93caebd657d34bb3e478
Joseph McGee <sonnyboyg1@gmail.com>
2012-05-07 18:07:02 -0700

    Jae forgot something when he changed cbccore to cbc

--- src/utils/vision/Blob.java
+++ src/utils/vision/Blob.java
@@ -2,7 +2,7 @@ package utils.vision;
 
 import cbc.sensors.vision.Camera;
 
-public class Blob extends cbccore.sensors.vision.Blob{
+public class Blob extends cbc.sensors.vision.Blob{
 
 	public Blob(int ch, int index) {
 		super(new Camera(), ch, index);
